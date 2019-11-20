11ac71f95c56aa91217faeb834e02d0ff9f73067
Iz-Dine Amadou <2088346@talnet.nl>
2019-11-20 15:20:05 +0100

    Uitwerking van de exercise

---
+++ hello.php
@@ -0,0 +1,32 @@
+<?php
+$a = "dit is een String";
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+var_dump ($a);
+
+
+
+
+ ?>
